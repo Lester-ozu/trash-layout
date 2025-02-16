5afc774c6f6ffb9610ccd4515c8cb60683831998
Lester-ozu <jlc628549@gmail.com>
2025-02-16 22:44:12 +0800

    Version 1.0.7

--- historyJS.js
+++ historyJS.js
@@ -1,3 +1,12 @@
+window.addEventListener("load", () => {
+
+    let scrollTop = window.scrollY;
+    let pageHeight = document.documentElement.scrollHeight - window.innerHeight;
+    let scrollIndex = Math.floor((scrollTop / pageHeight) * 8);
+
+    document.body.style.backgroundImage = `url('${scrollIndex}.jpg')`
+});
+
 window.addEventListener("scroll", () => {
 
     let scrollTop = window.scrollY;
