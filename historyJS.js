7557f1cf6a5815fcaba353b9501250c3877ad347
Lester-ozu <jlc628549@gmail.com>
2025-02-16 22:44:12 +0800

    Version 1.0.8

--- historyJS.js
+++ historyJS.js
@@ -1,10 +1,18 @@
-window.addEventListener("scroll", () => {
+const imageCount = 8;
+const images = Array.from({length: imageCount + 1}, (_, i) => new Image());
+images.forEach((img, i) => img.src = `${i}.jpg`);
 
-    let scrollTop = window.scrollY;
-    let pageHeight = document.documentElement.scrollHeight - window.innerHeight;
-    let scrollIndex = Math.floor((scrollTop / pageHeight) * 8);
+const updateBackground = () => {
 
-    document.body.style.backgroundImage = `url('${scrollIndex}.jpg')`
-});
+    const scrollTop = window.scrollY;
+    const pageHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollIndex = Math.min(
+        Math.floor((scrollTop / pageHeight) * imageCount),
+        imageCount
+    );
 
-console.log("hi")
+    document.body.style.backgroundImage = `url('${scrollIndex}.jpg')`;
+}
+
+window.addEventListener("load", updateBackground);
+window.addEventListener("scroll", updateBackground);
